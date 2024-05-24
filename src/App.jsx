b34c04d40e975714ef9adfa735f0182953b8d62f
João Vitor <joaovitor55670@gmail.com>
2024-05-24 11:52:24 -0300

    em andamento

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 // Components
 import Button from './Components/Button/Button'
 import Quiz from './Components/Quiz/Quiz'
+import FinalPage from './Components/FinalPage/FinalPage'
 
 // Datas
 import { BUTTONS_INFOS } from './data'
@@ -16,18 +17,36 @@ function App() {
   const [currentChoice, setcurrentChoice] = useState(undefined)
   const [currentIndex, setcurrentIndex] = useState(0)
   const [correctChoice, setcorrectChoice] = useState(undefined)
+  const [count, setCount] = useState(0)
+
   let elementQuiz = ''
+  let screens = undefined
+
 
   const buttonChoice = (e) => {
     setcurrentChoice(e.target.innerText)
+    console.log(e)
   }
 
-  const defineCorrect = () => {
+  const defineCorrect = (e) => {
+    if (+e.target.id === elementQuiz.correct) {
+      setCount(count + 1)
+    }
     setcorrectChoice(elementQuiz.correct)
   }
 
   const nextElement = () => {
-    setcurrentIndex(currentIndex + 1)
+    if ((currentIndex + 1) > 4) {
+      screens = <FinalPage title={currentChoice} onClick={resetQuiz} count={count} />
+    } else {
+      setcurrentIndex(currentIndex + 1)
+      setcorrectChoice(undefined)
+    }
+  }
+
+  const resetQuiz = () => {
+    setcurrentChoice(undefined)
+    setcurrentIndex(0)
     setcorrectChoice(undefined)
   }
 
@@ -35,28 +54,33 @@ function App() {
     elementQuiz = QUESTIONS_DATA[currentChoice][currentIndex]
   }
 
+  if (!currentChoice) {
+    screens = <>
+      <div className="left">
+        <div className="content-left">
+          <h1>Bem vindo ao Quiz!</h1>
+          <label>Escolha um dos Assuntos para começarmos</label>
+        </div>
+      </div>
+      <div className="right">
+        {BUTTONS_INFOS.map((info) => <Button key={info.title} bgColor={info.bgColor} onClick={buttonChoice} >{info.title}</Button>)}
+      </div>
+    </>
+  } else {
+    screens = <Quiz title={currentChoice} question={elementQuiz.question} button={correctChoice === elementQuiz.correct} onClickButton={nextElement}>
+      {elementQuiz.options.map((option, indx) => <li onClick={defineCorrect} id={indx} style={{ background: correctChoice == indx && '#14e548' }}>{option}</li>)}
+    </Quiz>
+  }
+
+  // if ((currentIndex + 1) > 4) {
+  //   screens = <FinalPage title={currentChoice} onClick={resetQuiz} count={count} />
+  // }
+
+
   return (
     <>
       <div className="container-start" style={currentChoice && { flexDirection: 'column', justifyContent: 'center' }}>
-        {!currentChoice ?
-          <>
-            <div className="left">
-              <div className="content-left">
-                <h1>Bem vindo ao Quiz!</h1>
-                <label>Escolha um dos Assuntos para começarmos</label>
-              </div>
-            </div>
-            <div className="right">
-              {BUTTONS_INFOS.map((info) => <Button key={info.title} bgColor={info.bgColor} onClick={buttonChoice} >{info.title}</Button>)}
-            </div>
-          </>
-          :
-          <>
-
-            <Quiz title={currentChoice} question={elementQuiz.question} button={correctChoice === elementQuiz.correct} onClickButton={nextElement}>
-              {elementQuiz.options.map((option, indx) => <li onClick={defineCorrect} style={{ background: correctChoice == indx && '#14e548' }}>{option}</li>)}
-            </Quiz>
-          </>}
+        {screens}
       </div>
     </>
   )
---
+++ src/Components/FinalPage/FinalPage.jsx
@@ -0,0 +1,21 @@
+import emojiIcon from '../../assets/ok.png'
+import './FinalPage.css'
+
+
+const FinalPage = ({ hits, title, onClick, count }) => {
+
+
+
+    return (
+        <div className="final-container">
+            <h2>Quiz de {<a className={title}>{title}</a>} Finalizado!</h2>
+            <div>
+                <p>Muito obrigado por participar!</p>
+                <p><a className={title}>Você acertou {count} de 4 questões!</a></p>
+            </div>
+            <button onClick={onClick} className={title}>Jogar novamente</button>
+        </div>
+    )
+}
+
+export default FinalPage
